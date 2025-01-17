5ca0244465cd554aed505e2ddcb52bc40cb8900d
MWebb35 <115995439+MWebb35@users.noreply.github.com>
2025-01-17 12:55:04 +0000

    Create homepage.h

---
+++ ESP32_POC_Webpages/homepage.h
@@ -0,0 +1,67 @@
+const char homepage[] PROGMEM = R"=====(
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>homepage</title>
+
+    <style>
+    html{
+        background-color: black;
+    }
+
+    h1{
+        color: white;
+        font-size: 50;
+    }
+
+    .flexbox{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+
+    button {
+    background-color: #3450C3;
+    border: none;
+    color: white;
+    padding: 16px 40px;
+    border-radius: 10px;
+    text-decoration: none;
+    font-size: 30px;
+    cursor: pointer;}
+
+    a {
+      text-decoration: none;
+      color: white;
+    } 
+
+    /*Used to create circles*/
+    .dot {
+      height: 300px;
+      width: 300px;
+      background-color: #bbb;
+      border-radius: 50%;
+      display: inline-block;
+    }
+
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="flexbox">Smart Glasses</h1>
+
+        <p>
+            <button class="flexbox"><a href="/login">Login</a></button>
+        </p>
+
+
+
+    </div>
+</body>
+
+</html>
+)=====";
