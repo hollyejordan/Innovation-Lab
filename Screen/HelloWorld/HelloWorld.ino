577e140b3f690bf252b343e691c6d9638c76f824
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-01-20 18:11:35 +0000

    Get recorded data from the websocket and display it on the ESP's screen

--- Screen/HelloWorld/HelloWorld.ino
+++ Screen/HelloWorld/HelloWorld.ino
@@ -1,17 +1,85 @@
-#include <TFT_eSPI.h>  // Include the TFT_eSPI library
+#include <ArduinoWebsockets.h>
+#include <WiFi.h>
+#include <TFT_eSPI.h>
 
 TFT_eSPI tft = TFT_eSPI();  // Create TFT object
 
-void setup() {
+const char *ssid = "Dnt";                              // Enter SSID
+const char *password = "bingus123";                    // Enter Password
+const char *websockets_server = "ws://192.168.243.90:9067"; // server adress and port
+
+using namespace websockets;
+
+void onMessageCallback(WebsocketsMessage message)
+{
+  Serial.print("Got Message: ");
+  Serial.println(message.data());
+
+  String text = message.data().substring(18, message.data().length() - 18);
+  text = text.substring(0, text.indexOf('"'));
+
+  tft.fillScreen(TFT_BLACK);  // Fill the screen with black color
+  tft.setCursor(10, 10);      // Set the cursor position
+  tft.print(text);
+}
+
+void onEventsCallback(WebsocketsEvent event, String data)
+{
+  if (event == WebsocketsEvent::ConnectionOpened)
+  {
+    Serial.println("Connnection Opened");
+  }
+  else if (event == WebsocketsEvent::ConnectionClosed)
+  {
+    Serial.println("Connnection Closed");
+  }
+  else if (event == WebsocketsEvent::GotPing)
+  {
+    Serial.println("Got a Ping!");
+  }
+  else if (event == WebsocketsEvent::GotPong)
+  {
+    Serial.println("Got a Pong!");
+  }
+}
+
+WebsocketsClient client;
+void setup()
+{
+  Serial.begin(115200);
+
   tft.begin();              // Initialize the TFT screen
   tft.setRotation(3);       // Set rotation (optional)
   tft.fillScreen(TFT_BLACK);  // Fill the screen with black color
   tft.setTextColor(TFT_WHITE);  // Set text color to white
-  tft.setTextSize(2);         // Set text size
+  tft.setTextSize(4);         // Set text size
   tft.setCursor(10, 10);      // Set the cursor position
-  tft.print("Hello, World!"); // Print a test message
+  tft.print("Welcome."); // Print a test message
+
+  // Connect to wifi
+  WiFi.begin(ssid, password);
+
+  // Wait some time to connect to wifi
+  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  // Setup Callbacks
+  client.onMessage(onMessageCallback);
+  client.onEvent(onEventsCallback);
+
+  // Connect to server
+  client.connect(websockets_server);
+
+  // Send a message
+  client.send("Hi Server!");
+  // Send a ping
+  client.ping();
 }
 
-void loop() {
-  // Your code here
+void loop()
+{
+  client.poll();
 }
