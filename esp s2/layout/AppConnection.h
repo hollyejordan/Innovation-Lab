dea634983186d2ed4088c88a78f348fc2493e4db
louis2374 <115995518+louis2374@users.noreply.github.com>
2025-02-21 12:16:05 +0000

    Add basic class layout

---
+++ esp s2/layout/AppConnection.h
@@ -0,0 +1,58 @@
+#pragma once
+
+// Placeholder
+typedef int Buffer;
+
+// The data format the websocket uses, converted to a C++ struct
+typedef int WSMessage;
+typedef int WebSocket;
+
+// For now this class could handle
+// Websocket - audio, incoming transcriptions, actions such as settings change from app
+// Initial connection - Connecting to hotspot on phone, (still not sure the best method to do this without
+// hardcoding)
+class AppConnection
+{
+	// If the ESP is connected to the app (not websocket)
+	bool network_connected;
+
+
+	// Websocket will be its own class
+	WebSocket* socket;
+
+
+
+public:
+
+	// Input & output
+
+	// Send audio buffer to the app, should be synchronous and return true once its complete
+	// false if it failed. The buffer should be locked until this is complete
+	bool app_send_buffer(const Buffer* p_buffer);
+
+	// Sends a message to the app, false if failed
+	bool app_send_message(const WSMessage& p_message);
+	
+	// When the app sends a message, could be a setting change or incoming text transcription
+	bool on_received_message(const WSMessage& p_message);
+
+	// If both network and websocket have connected and can communicate
+	bool is_connected();
+
+	// Disconnect from the app
+	void disconnect();
+	
+	// No idea how this will work, just suggestion
+	// Scans the available networks until it finds the correct one
+	// This could be the app using a hardcoded temp name or something (not secure)
+	// Returns false if it timed out / somehow failed
+	bool look_for_connection();
+
+
+	// Loop required by lib
+	void loop();
+
+	// WebSocket will need to be deleted, connections closed and ect
+	~AppConnection();
+};
+
---
+++ esp s2/layout/ScreenManager.h
@@ -0,0 +1,91 @@
+#pragma once
+
+// Placeholder
+typedef int String;
+typedef int DiarizedText;
+typedef int RGBColor;
+
+
+// Settings, placeholder
+struct ScreenManagerSettings
+{
+	// Defaults
+	unsigned char font_size = 5;
+
+	// If we have diff color modes for accessibility
+	unsigned char color_scheme = 1;
+
+	// Text must be displayed for at least this time (May not be necessary, but I think it may be)
+	// Max ~32k
+	unsigned short min_display_time = 4000;
+
+//32
+
+	// Just to play around with. Probably not needed tbh
+	unsigned char margin = 0;
+
+	// Wrapping
+	bool word_break = true;
+
+	// Break words with a dash
+	bool dash_break = false;
+
+	// If the screen should only display one speaker at a time
+	bool single_speaker = false;
+
+//64
+};
+
+
+// This manages the screen & display logic, such as when the next text is displayed,
+// how long for, coloring and ect
+class ScreenManager
+{
+	// Im not sure of the classes and ect required to control the screen, but they go here
+	/*
+	
+	*/
+
+	ScreenManagerSettings settings{};
+
+
+	// Util funcs to get screen info //
+	// May be moved out of the class
+
+	// Gets the number of lines without any text, based on font size
+	void screen_get_lines_free();
+
+	// Gets the number of lines a string of text will take up, once it has been wrapped with the
+	// current wrapping settings, accounting for margin & fontsize
+	void text_wrapped_get_lines();
+
+
+	// Low level functions for controlling the screen //
+
+	// Directly sets the text. Should clear the screen first, so that only p_text is displayed
+	void screen_set_text(const String& p_text);
+
+	// Adds text to the screen
+	void screen_push_text(const String& p_text, const RGBColor& p_color);
+
+	// Completely clears the screen
+	bool screen_clear();
+
+public:
+	// Screen probably has some setup stuff idk
+	void init();
+
+	// Queues text to be displayed, will be managed with min display time
+	// Still unsure whether this logic will be in this class or not
+	void queue_text(const DiarizedText& p_text);
+
+	// For other use cases where the text must be set, such as loading or pairing
+	void set_text(const String& p_text);
+
+
+
+
+
+
+};
+
