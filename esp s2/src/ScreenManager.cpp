7a1539c13d732af1b0b363f704186c45e05a9f11
hollyejordan <131594827+hollyejordan@users.noreply.github.com>
2025-03-04 17:39:16 +0000

    Update ScreenManager init()
    Removed unnecessary code from the init() method.

--- esp s2/src/ScreenManager.cpp
+++ esp s2/src/ScreenManager.cpp
@@ -5,28 +5,20 @@ void ScreenManager::init() {
     delay(250); // Wait for the screen to power up
     display.begin(0x3C, true); // Address 0x3C default
 
-    // Clear the buffer
-    display.clearDisplay();
-    display.display();
-
     // Set text settings
     display.setRotation(1);
     display.setTextSize(1);
     display.setTextColor(SH110X_WHITE);
-    display.setCursor(0,0);
 
-    display.println("Welcome."); // Display welcome message
-    display.display(); // Update screen to display changes
+    this->screen_set_text("Welcome"); // Display a welcome message
 }
 
-/*
 void ScreenManager::screen_set_text(const String& p_text) {
 
-    tft.fillScreen(TFT_BLACK); // Sets the screen to black
-    tft.setCursor(10, 10); // Reset the cursor
-    tft.print(p_text); // Print the text to the screen
+    this->screen_clear(); // Clear and reset the screen
+    display.println(p_text); // Display the text on the screen
+    display.display();
 }
-*/
 
 void ScreenManager::screen_clear() {
 
